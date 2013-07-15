3351cc295c7400ad7e92232c0ac07fe54d38aa08
MakerSquare Student <students@themakersquare.com>
2013-07-15 15:15:25 -0500

    created separate sub-folders for js tasks.

---
+++ Part1/appQ1-12.js
@@ -0,0 +1,41 @@
+// #4 
+alert("Hi");
+
+// #5
+name = prompt("What is your name?");
+console.log("Nice to meet you, " + name);
+
+// #6
+// movie = prompt("What movie do you like?");
+// time = prompt("What time would you like?");
+// answer = prompt("Great! Would you like to go see " + movie + " with me tonight at " + time + "?")
+// console.log(answer);
+
+// #7
+// pw = prompt("Please enter a password");
+// while(pw.length <= 6){
+//   alert("Password must be 6 characters or longer");
+//   pw = prompt("Please enter a different password");
+// }
+// console.log("Success!")
+
+// #8
+// age = prompt("What is your age?");
+// if (age>=21){
+//   alert("Yay! Get loaded!")
+// }
+// else{
+//   alert("Sorry, try again next year :-(")
+// }
+
+// #9-11 remaining
+
+
+
+
+
+
+
+
+
+
---
+++ Part2/app_Q13.js
@@ -0,0 +1,77 @@
+// #############################################
+// USING SELECTORS
+// #############################################
+
+// ############
+// 16 
+// ############
+// Select an h1 header:  
+// $('h1')
+
+// Select all the divs:
+// $('div')
+
+// Select the first div:
+// $('div:first')
+
+// /Select the profile picture using it's class:
+// $('.profilepic')
+
+// Select all nav-items using it's class:
+// $('.nav-item')
+
+// Select an object using it's id:
+// $('#bottom-nav-item')
+
+// Try 3 new types of selectors:
+// $('[href]')
+// $("a[target!='_blank']")
+// $("ul li:first")
+// $("ul li:first-child")
+
+// Use .parent(), .children(), and .sibling() to grab elements near the one you've selected
+// $('ul').parent()
+// $('ul').children()
+// $('li').siblings()
+
+// ############
+// 17
+// ############
+// $('.twitter-timeline').hide()
+
+// ############
+// 18
+// ############
+// $('.card').hide()
+
+// ############
+// 19
+// ############
+// $('.main').show()
+
+// ##############
+// 20
+// ##############
+
+
+
+
+
+
+// ##########################################
+// EVENT HANDLING
+// ##########################################
+
+// ##############
+// 31
+// ##############
+
+
+
+
+
+
+
+
+
+
