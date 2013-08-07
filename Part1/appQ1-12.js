fd9cb146b45df743ccd49be6f1ac95897f46fa5d
MakerSquare Student <students@themakersquare.com>
2013-08-07 15:46:51 -0500

    didn't change anythign important

--- Part1/appQ1-12.js
+++ Part1/appQ1-12.js
@@ -1,98 +1,98 @@
-// // #4 
-// alert("Hi");
+// #4 
+alert("Hi");
 
-// // #5
-// name = prompt("What is your name?");
-// console.log("Nice to meet you, " + name);
+// #5
+name = prompt("What is your name?");
+console.log("Nice to meet you, " + name);
 
-// #6
-// movie = prompt("What movie do you like?");
-// time = prompt("What time would you like?");
-// answer = prompt("Great! Would you like to go see " + movie + " with me tonight at " + time + "?")
-// console.log(answer);
+#6
+movie = prompt("What movie do you like?");
+time = prompt("What time would you like?");
+answer = prompt("Great! Would you like to go see " + movie + " with me tonight at " + time + "?")
+console.log(answer);
 
-// #7
-// pw = prompt("Please enter a password");
-// while(pw.length <= 6){
-//   alert("Password must be 6 characters or longer");
-//   pw = prompt("Please enter a different password");
-// }
-// console.log("Success!")
+#7
+pw = prompt("Please enter a password");
+while(pw.length <= 6){
+  alert("Password must be 6 characters or longer");
+  pw = prompt("Please enter a different password");
+}
+console.log("Success!")
 
-// #8
-// age = prompt("What is your age?");
-// if (age>=21){
-//   alert("Yay! Get loaded!")
-// }
-// else{
-//   alert("Sorry, try again next year :-(")
-// }
+#8
+age = prompt("What is your age?");
+if (age>=21){
+  alert("Yay! Get loaded!")
+}
+else{
+  alert("Sorry, try again next year :-(")
+}
 
-// #9
-// Ruby:
-// grocery_list = [bananas, apples, strawberries, spinach]
-// grocery_list.each do |item|
-// 	puts item 
-// end
-// puts "You're done shopping!"
+#9
+Ruby:
+grocery_list = [bananas, apples, strawberries, spinach]
+grocery_list.each do |item|
+	puts item 
+end
+puts "You're done shopping!"
 
-// // JS & Jquery:
-// grocery_list = ["bananas", "apples", "strawberries", "spinach"]
-// $.each(grocery_list, function(i, item){
-//     console.log('You need to buy ' + item);
-//   }
-//   );
-// console.log("Youre done shopping!")
+// JS & Jquery:
+grocery_list = ["bananas", "apples", "strawberries", "spinach"]
+$.each(grocery_list, function(i, item){
+    console.log('You need to buy ' + item);
+  }
+  );
+console.log("Youre done shopping!")
 
-// # // #10
-// user = new Object()
-// user.name = "Rachel"
-// user.age = "33"
-// user.gender = "f"
-// user.height = "5ft 3in"
+# // #10
+user = new Object()
+user.name = "Rachel"
+user.age = "33"
+user.gender = "f"
+user.height = "5ft 3in"
 
-// // or....
-// user = {
-//   name: "Rachel"
-//   age: "33"
-//   gender: "f"
-//   height: "5ft 3in"
-// }
+// or....
+user = {
+  name: "Rachel"
+  age: "33"
+  gender: "f"
+  height: "5ft 3in"
+}
 
-// #11
-// // # TODO: Create a h1 header with the person's name in it
-// create_header = function(name){
-//   document.write('<h1>' + name + '</h1>')
-// }
-// create_header('Rachel')
+#11
+// # TODO: Create a h1 header with the person's name in it
+create_header = function(name){
+  document.write('<h1>' + name + '</h1>')
+}
+create_header('Rachel')
 
-// // or..
+// or..
 
-// function create_header(name){
-//   document.write('<h1>' + name + '</h1>')
-// }
+function create_header(name){
+  document.write('<h1>' + name + '</h1>')
+}
 
-// create_header('Rachel')
+create_header('Rachel')
 
-// // # TODO: Create an image tag with the image_file being the source
-// function create_image(img_file){
-// 	document.write("<img src='" + img_file + "'>")
-// }
-// create_image('Austin&Rachel.jpeg')
+// # TODO: Create an image tag with the image_file being the source
+function create_image(img_file){
+	document.write("<img src='" + img_file + "'>")
+}
+create_image('Austin&Rachel.jpeg')
 
-// // # TODO: Create a paragraph tag using the arguments age, gender, and job
-// function create_paragraph(age, gender, job){
-// 	document.write("<p>Age: " + String(age) + "; Gender: " + gender + "; Job: " + job + "</p>");
-// };
+// # TODO: Create a paragraph tag using the arguments age, gender, and job
+function create_paragraph(age, gender, job){
+	document.write("<p>Age: " + String(age) + "; Gender: " + gender + "; Job: " + job + "</p>");
+};
 
-// create_paragraph(33, 'female', 'Web Developer');
+create_paragraph(33, 'female', 'Web Developer');
 
-//  // # TODO: By calling the above 3 methods, print out the person's profile information
-//  function create_profile(create_header, create_image, create_paragraph){
-//  	console.log(create_header);
-//  	console.log(create_image);
-//  	console.log(create_paragraph);
-//  };
+ // # TODO: By calling the above 3 methods, print out the person's profile information
+ function create_profile(create_header, create_image, create_paragraph){
+ 	console.log(create_header);
+ 	console.log(create_image);
+ 	console.log(create_paragraph);
+ };
 
 
 
